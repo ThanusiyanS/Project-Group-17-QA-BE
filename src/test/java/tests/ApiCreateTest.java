63117803bfda570d9d41d1d8fa1828ac6d04461f
ThanusiyanS <thanusiyan295@gmail.com>
2024-12-25 21:34:52 +0530

    Initial Get request test cases added

---
+++ src/test/java/tests/ApiCreateTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class ApiCreateTest {
+
+
+
+    @Test
+    public void testAddBookWithAdmin() {
+        // Admin credentials
+        String newBook = "{\"title\": \"New Book 8\", \"author\": \"Nick P\"}";
+
+        given()
+                .auth().preemptive().basic("admin", "password")
+                .contentType(ContentType.JSON)
+                .body(newBook)
+                .when()
+                .post("/books")
+                .then()
+                .statusCode(201);
+    }
+
+
+}
---
+++ src/test/java/tests/BaseApiTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseApiTest {
+    @BeforeSuite
+    public void setup() {
+        // Set the base URI for all tests
+        RestAssured.baseURI = "http://localhost:7081/api";
+    }
+}
