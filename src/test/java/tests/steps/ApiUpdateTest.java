afcd4d4af6570716d0da5184f68a12024b92db36
Mahilanmx23 <you@example.com>
2025-01-03 18:37:56 +0530

    update book testcases

--- src/test/java/tests/steps/ApiUpdateTest.java
+++ src/test/java/tests/steps/ApiUpdateTest.java
@@ -1,13 +1,14 @@
 package tests.steps;
 
-
-import io.cucumber.java.en.And;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.And;
 import io.qameta.allure.Allure;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.example.DTO.Book;
 
 import java.util.Map;
 
@@ -19,57 +20,97 @@ public class ApiUpdateTest {
 
     private String endpoint;
     private Response response;
-    private Map<String, String> updateData;
+    private Map<String, String> requestData;
 
+
+    public Book getData(String type){
+        switch (type) {
+            case "validData" :
+                return new Book(1,"The Great ", "F. Scott Fitzgerald");
+            case "invalidData" :
+                return new Book(4,null,"mee");
+            default:
+                return null;
+        }
+    }
 
     @Given("the API endpoint {string} for updating a book")
-    public void theApiEndpoint(String endpoint) {
+    public void setApiEndpoint(String endpoint) {
         this.endpoint = endpoint;
     }
 
+    @When("a PUT request is sent with the {string}")
+    public void sendPutRequestWithDataX(String type) {
+//        this.requestData = data;
+//        System.out.println(data);
+        Book validData = new Book(1,"The Great ", "F. Scott Fitzgerald");
 
-    @When("a PUT request is sent with the following data:")
-    public void sendPutRequestWithData(Map<String, String> data) {
-        // Save the update data for validation
-        this.updateData = data;
-
-        // Send the PUT request with basic authentication and JSON body
+        // Sending a PUT request with basic authentication as a regular user
         response = RestAssured.given()
                 .auth()
                 .preemptive()
-                .basic("user", "password") // Replace with valid credentials
+                .basic("admin", "password") // Replace with valid credentials
                 .header("Content-Type", "application/json")
-                .body(data)
+                .body(getData(type))
                 .when()
                 .put(endpoint);
     }
 
+    @When("a PUT request is sent with the following data:")
+    public void sendPutRequestWithData(DataTable dataTable) {
+        var bookData = dataTable.asMaps(String.class, String.class).get(0);
 
-    @Then("the response status code of update should be {int}")
-    public void theResponseStatusCodeIs(int statusCode) {
-        // Assert that the response status code matches the expected value
-        assertThat(response.getStatusCode(), equalTo(statusCode));
+        System.out.println(bookData);
+
+        // Sending a PUT request with basic authentication as a regular user
+        response = RestAssured.given()
+                .auth()
+                .preemptive()
+                .basic("admin", "password") // Replace with valid credentials
+                .header("Content-Type", "application/json")
+                .body(bookData)
+                .when()
+                .put(endpoint);
     }
 
+    @And("the user is logged in as a regular user")
+    public void userIsLoggedInAsRegularUser() {
+        // This step is implicitly handled by the authentication in the request.
+        System.out.println("User is authenticated as a regular user.");
+    }
+
+    @Then("the response status code of update should be {int}")
+    public void verifyResponseStatusCode(int expectedStatusCode) {
+        assertThat(response.getStatusCode(), equalTo(expectedStatusCode));
+    }
+
+    @And("the response message should indicate {string}")
+    public void verifyResponseMessage(String expectedMessage) {
+        response.then()
+                .assertThat()
+                .body("error", equalTo(expectedMessage));
+
+        // Debug log for verification
+        System.out.println("Response: " + response.getBody().asString());
+    }
 
     @Then("the response should contain updated details")
     public void verifyUpdatedDetails() {
         // Verify the response body contains the updated title and author
         response.then()
                 .assertThat()
-                .body("title", equalTo(updateData.get("title")))
-                .body("author", equalTo(updateData.get("author")));
+                .body("title", equalTo(requestData.get("title")))
+                .body("author", equalTo(requestData.get("author")));
 
         // Log the validation details in Allure reports
         Allure.addAttachment(
                 "Updated Details",
-                "Verified that the response contains updated title: " + updateData.get("title") +
-                        " and author: " + updateData.get("author")
+                "Verified that the response contains updated title: " + requestData.get("title") +
+                        " and author: " + requestData.get("author")
         );
     }
 
-    @And("the response message should indicate {string}")
-    public void theResponseMessageShouldIndicate(String arg0) {
+    @Then("the response status code of update should be {int}")
+    public void theResponseStatusCodeOfUpdateShouldBe(int arg0) {
     }
 }
-
--- src/test/resources/features/updatebooks.feature
+++ src/test/resources/features/updatebooks.feature
@@ -1,29 +1,92 @@
 Feature: Update Book API Testing
+#
+##  Scenario: Attempt to update a book with invalid characters in the title
+##Given the API endpoint "/books/1" for updating a book
+##When a PUT request is sent with the following data:
+##| title  | @Invalid!#Title$% |
+##| author | Valid Author       |
+##Then the response status code of update should be 401
+##
+##
+##Scenario: Update a book without providing the author field
+##Given the API endpoint "/books/1" for updating a book
+##When a PUT request is sent with the following data:
+##| title  | Valid Title |
+##Then the response status code of update should be 401
+##
+##
+##Scenario: Update a book without providing the title field
+##Given the API endpoint "/books/1" for updating a book
+##When a PUT request is sent with the following data:
+##| author | Valid Author |
+##Then the response status code of update should be 401
+##
+##
+#
+#
+## mahilan
+
+
+##
+
+  Scenario: Attempt to update a book with invalid data
+    Given the API endpoint "/books/3" for updating a book
+    When a PUT request is sent with the following data:
+      | id | title      | author        |
+      |    |            |              |
+    Then the response status code of update should be 400
+###    And the response message should indicate "Invalid data provided"
+
 
   Scenario: Update a book with valid data
     Given the API endpoint "/books/1" for updating a book
     When a PUT request is sent with the following data:
-      | title  | Updated Book Title |
-      | author | Updated Author     |
+    | id | title      | author        |
+    | 1  | Gone Girl  | Gillian Flynn |
     Then the response status code of update should be 200
-    And the response should contain updated details
 
-
-
-  Scenario: Attempt to update a non-existing book
+  Scenario: Update a book with a valid id that does not exist in the database
+    Given the API endpoint "/books/999" for updating a book
+    When a PUT request is sent with the following data:
+      | id | title      | author        |
+      | 999  | Gone Girl  | Gillian Flynn |
+    Then the response status code of update should be 404
+@bug_u1
+  Scenario: Attempt to update a book with invalid characters in the title
     Given the API endpoint "/books/1" for updating a book
     When a PUT request is sent with the following data:
-      | title  | Non-Existent Title |
-      | author | Non-Existent Author |
+      | id   | title             | author        |
+      | 1    | @Invalid!#Title$%  | Valid Author  |
     Then the response status code of update should be 400
 
-  Scenario: Attempt to update a book with invalid data
-    Given the API endpoint "/books/1" for updating a book
+  Scenario: Update a book without providing the author field
+    Given the API endpoint "/books/2" for updating a book
     When a PUT request is sent with the following data:
-      | title  |          |
-      | author | Invalid Author Name |
+      | id   | title        | author |
+      | 2    | Valid Title  |        |
     Then the response status code of update should be 400
-    And the response message should indicate "Invalid data provided"
 
+  Scenario: Update a book without providing the id
+    Given the API endpoint "/books/ " for updating a book
+    When a PUT request is sent with the following data:
+      | id   | title             | author        |
+      |      | Valid title       | Valid Author  |
+    Then the response status code of update should be 400
+
+  Scenario: Attempt to update a book by an unauthorized user
+    Given the API endpoint "/books/3" for updating a book
+    When a PUT request is sent with the following data:
+      | id   | title          | author         |
+      | 3    | Updated Title  | Updated Author |
+    And the user is logged in as a regular user
+    Then the response status code of update should be 401
+
+#  Scenario: Attempt to update a book with invalid characters in the title
+#  Given the API endpoint "/books/1" for updating a book
+#  When a PUT request is sent with the following data:
+#    | id  | 1 |
+#  | title  | @Invalid!#Title$% |
+#  | author | Valid Author       |
+#  Then the response status code of update should be 401
 
 
